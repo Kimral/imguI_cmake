e4aacd32f6f68846d50b706428e2dce1d08a8209
Kimral <semaytes2012@mail.ru>
2025-04-01 20:59:49 +0500

    fix

--- cmake/LinkLibs.cmake
+++ cmake/LinkLibs.cmake
@@ -1,6 +1,6 @@
 if(${IMGUI_BACKEND_SDL2})
     message("SDL2 LINKED")
-    target_link_libraries(imgui PUBLIC SDL2::SDL SDL2::SDL2main)
+    target_link_libraries(imgui PUBLIC SDL2::SDL2 SDL2::SDL2main)
 endif()
 
 if(${IMGUI_RENDER_OPENGL3})
--- cmake/dependencies/sdl2.cmake
+++ cmake/dependencies/sdl2.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
     sdl2
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
     GIT_TAG release-2.32.x
+    GIT_PROGRESS TRUE
 )
 
 FetchContent_MakeAvailable(sdl2)
