ba1fe6808a8da86a43cb85deb892903de1bce07e
Kimral <semaytes2012@mail.ru>
2025-04-09 17:52:00 +0500

    CMAKE_BUILD_TYPE

--- cmake/dependencies/sdl2.cmake
+++ cmake/dependencies/sdl2.cmake
@@ -3,6 +3,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
     GIT_TAG release-2.32.x
     GIT_PROGRESS TRUE
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 )
 
 FetchContent_MakeAvailable(sdl2)
